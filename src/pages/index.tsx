e8e9e6afdf7a39fa887938829d34f6b22d458b71
Andre <sgavsnake@gmail.com>
2023-12-05 00:15:25 -0300

    mod 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import Primeiro from "@/components/basicos/primeiro";
 import PrimeiraPagina from "./basicos/primeiro";
 import PaginaImagens from "./hooks/imagem";
 
 export default function Home() {
   return (
-    <PaginaImagens />    
+    <PaginaImagens   />    
   )
 }
